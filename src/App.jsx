03beda3c5821db03869c42ddfbb682b0e4531b52
JulianBarbossa9 <julianbarbossa@hotmail.com>
2022-07-12 15:41:29 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,57 @@
-import { useState } from 'react'
-import Header from './Components/Header'
-
+import { useState } from "react";
+import Header from "./Components/Header";
+import NewFormPresupuesto from "./Components/NewFormPresupuesto";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
+  const [valor, setValor] = useState(0);
+  const [isValid, setIsValid] = useState(false);
+  const [newVenta, setNewVentana] = useState(false);
+  const [animarVentana, setAnimarVentana] = useState(false);
+  const [gastos, setGastos] = useState([]);
 
 
-  const [valor, setValor ] = useState(0)
-  const [ isValid, setIsValid ] = useState(false); 
+  const handleNuevoGasto = () => {
+    setNewVentana(true);
+
+    setTimeout(() => {
+      setAnimarVentana(true);
+    }, 500);
+  };
+
+  const guardarGastos = (gasto) => {
+    setGastos([...gastos,gasto]);
+  };
 
   return (
-    <Header 
-      valor={valor}
-      setValor={setValor}
-      isValid={isValid}
-      setIsValid={setIsValid}
-    />
-  )
+    <div>
+      <Header
+        valor={valor}
+        setValor={setValor}
+        isValid={isValid}
+        setIsValid={setIsValid}
+      />
+
+      {isValid && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevoGasto}
+            alt="nuevo gasto icono"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+
+      {newVenta && (
+        <NewFormPresupuesto
+          setNewVentana={setNewVentana}
+          animarVentana={animarVentana}
+          setAnimarVentana={setAnimarVentana}
+          guardarGastos={guardarGastos}
+        />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/NewFormPresupuesto.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import Mensaje from './Mensajes'
+import BtnCerrar from '../img/cerrar.svg'
+
+const NewFormPresupuesto = ({setNewVentana, animarVentana, setAnimarVentana, guardarGastos}) => {
+    
+    
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
+    const [mjsError, setMjsError] = useState('')
+    
+    const handleCerrar = () =>{
+        // console.log('cerrando...');
+        setAnimarVentana(false)
+        
+        setTimeout(() => {
+            setNewVentana(false)
+        },300)
+
+    }
+
+    //Validación formulario
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if([nombre, cantidad, categoria].includes('')){
+            setMjsError('Todos los campos son obligatorios')
+
+            setTimeout(() => {
+                setMjsError('')
+            },300)
+            return      
+        }
+        guardarGastos({nombre, cantidad, categoria})
+
+    }
+
+  
+    
+    return (  
+        <>
+            <div className="modal">
+                <div className="cerrar-modal">
+                    <img
+                        src={BtnCerrar}
+                        alt='Boton cerrar'
+                        onClick={handleCerrar}
+                    />
+                </div>
+
+                <form
+                    onSubmit={handleSubmit} 
+                    className={`formulario ${animarVentana ? "animar": 'cerrar'}`}>
+                    {mjsError && <Mensaje tipo='error'>{mjsError}</Mensaje> }
+                    <legend>Nuevo Gasto</legend>
+
+                    <div className='campo'>
+                        <label htmlFor='nombre'>Nombre Gasto</label>
+
+                        <input
+                            id='nombre'
+                            type='text'
+                            placeholder='Añada el nombre del gasto'
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
+                            // onChange={}
+                            />
+                    </div>
+
+                    <div className='campo'>
+                        <label htmlFor='cantidad'>Cantidad</label>
+
+                        <input
+                            id='cantidad'
+                            type='number'
+                            placeholder='Añada la cantidad del gasto Ej: 9000'
+                            value={cantidad}
+                            onChange={(e) => setCantidad(e.target.value)}
+                            // onChange={}
+                        />
+                    </div>
+
+                    <div className='campo'>
+                        <label htmlFor='categoria'>Categoria</label>
+
+                        <select 
+                            id="categoria"
+                            value={categoria}
+                            onChange={e => setCategoria(e.target.value)}
+                        >
+                            <option value=''>--Seleccione--</option>
+                            <option value='ahorro'>Ahorro</option>
+                            <option value='comida'>Comida</option>
+                            <option value='casa'>Casa</option>
+                            <option value='gastos'>Gastos Varios</option>
+                            <option value='ocio'>Ocio</option>
+                            <option value='salud'>Salud</option>
+                            <option value='subcripciones'>Subcripciones</option>
+
+                        </select>
+                    </div>
+
+                    <input 
+                        type='submit'
+                        value='Añadir Gasto'
+                    />
+                </form>
+
+    
+            </div>
+        </>
+    );
+}
+ 
+export default NewFormPresupuesto;
